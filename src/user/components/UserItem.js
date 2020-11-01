41fb605d49508bbb4a8b161236a6f7c73b98f521
sharjeelyunus <sharjeel924@gmail.com>
2020-11-01 15:15:51 +0500

    👌 IMPROVE: Styling App & More Components

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import Avatar from '../../shared/components/UIelements/Avatar';
+import Card from '../../shared/components/UIelements/Card';
 import './UserItem.css';
 
 const UsersItem = props => {
     return (
         <li className="user-item">
-            <div classNam="user-item__content">
-                <div className="user-item__image">
-                    <img src={props.image} alt={props.name} />
-                </div>
-                <div className="user-item__info">
-                    <h2>{props.name}</h2>
-                    <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-                </div>
-            </div>
+            <Card className="user-item__content">
+                <Link to={`/${props.id}/places`}>
+                    <div className="user-item__image">
+                        <Avatar image={props.image} alt={props.name} />
+                    </div>
+                    <div className="user-item__info">
+                        <h2>{props.name}</h2>
+                        <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
+                    </div>
+                </Link>
+            </Card>
         </li>
     );
 };
