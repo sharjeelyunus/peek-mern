bc8b2b1f51f7a41c232fb313e4ca01eff37bbd7f
sharjeelyunus <sharjeel924@gmail.com>
2020-11-02 14:07:52 +0500

    📦 NEW: Adding a Main Header

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,20 +1,23 @@
 import React from 'react';
 
 import UserItem from './UserItem';
+import Card from '../../shared/components/UIelements/Card';
 import './UsersList.css';
 
 const UsersList = props => {
     if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No Users Found!</h2>
+                <Card>
+                    <h2>No Users Found!</h2>
+                </Card>
             </div>
         );
     }
 
     return <ul className="users-list">
         {props.items.map(user => (
-            <UserItem 
+            <UserItem
                 key={user.id}
                 id={user.id}
                 image={user.image}
