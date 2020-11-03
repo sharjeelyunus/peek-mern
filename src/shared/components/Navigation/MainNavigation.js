c51bf8518fde65f5100e89fada3f5590f704d486
sharjeelyunus <sharjeel924@gmail.com>
2020-11-03 13:52:09 +0500

    👌 IMPROVE: Implementing a Basic Mobile Navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,22 +3,32 @@ import { Link } from 'react-router-dom'
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = props => {
-    return <MainHeader>
-        <button className="main-navigation__menu-btn">
-            <span />
-            <span />
-            <span />
-        </button>
-        <h1 className="main-navigation__title">
-            <Link to="/">Peek</Link>
-        </h1>
-        <nav>
-            <NavLinks />
-        </nav>
-    </MainHeader>
+    return (
+        <React.Fragment>
+            <SideDrawer>
+                <nav className="main-navigation__drawer-nav">
+                    <NavLinks />
+                </nav>
+            </SideDrawer>
+            <MainHeader>
+                <button className="main-navigation__menu-btn">
+                    <span />
+                    <span />
+                    <span />
+                </button>
+                <h1 className="main-navigation__title">
+                    <Link to="/">Peek</Link>
+                </h1>
+                <nav className="main-navigation__header-nav">
+                    <NavLinks />
+                </nav>
+            </MainHeader>
+        </React.Fragment>
+    )
 };
 
 export default MainNavigation;
