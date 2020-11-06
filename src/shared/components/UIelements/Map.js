ad6d954854a7dd59c552f2bbae42a2741bf8dfca
sharjeelyunus <sharjeel924@gmail.com>
2020-11-06 20:04:47 +0500

    👌 IMPROVE: rendering map with OpenLayers

--- src/shared/components/UIelements/Map.js
+++ src/shared/components/UIelements/Map.js
@@ -8,16 +8,27 @@ const Map = props => {
     const { center, zoom } = props;
 
     useEffect(() => {
-        const map = new window.google.maps.Map(mapRef.current, {
-            center: center,
-            zoom: zoom
+        new window.ol.Map({
+            target: mapRef.current.id,
+            layers: [
+                new window.ol.layer.Tile({
+                    source: new window.ol.source.OSM()
+                })
+            ],
+            view: new window.ol.View({
+                center: window.ol.proj.fromLonLat([center.lng, center.lat]),
+                zoom: zoom
+            })
         });
-
-        new window.google.maps.Marker({ position: center, map: map })
     }, [center, zoom]);
 
     return (
-        <div ref={mapRef} className={`map ${props.className}`} style={props.style}></div>
+        <div
+            ref={mapRef}
+            className={`map ${props.className}`}
+            style={props.style}
+            id="map"
+        ></div>
     );
 };
 
