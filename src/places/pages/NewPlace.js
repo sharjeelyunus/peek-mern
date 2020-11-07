8c27814acdcd818b85ddb49a20777dade36cd554
sharjeelyunus <sharjeel924@gmail.com>
2020-11-07 16:39:08 +0500

    👌 IMPROVE: Managing State in the Input Component

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -6,7 +6,13 @@ import './NewPlace.css';
 const NewPlace = () => {
   return (
     <form className="place-form">
-      <Input element="input" type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please Enter a valid title."
+      />
     </form>
   );
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './Input.css';
 
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'CHANGE':
+            return {
+                ...state,
+                value: action.val,
+                isValid: true
+            };
+        default:
+            return state;
+    }
+};
+
 const Input = props => {
+    const [inputState, dispatch] = useReducer(inputReducer, { value: '', isValid: false });
+
+    const changeHandler = event => {
+        dispatch({ type: 'CHANGE', val: event.target.value });
+    };
+
     const element =
         props.element === 'input' ? (
-            <input id={props.id} type={props.type} placeholder={props.placeholder} />
+            <input
+                id={props.id}
+                type={props.type}
+                placeholder={props.placeholder}
+                onChange={changeHandler}
+                value={inputState.value}
+            />
         ) : (
-                <textarea id={props.id} rows={props.rows || 3} />
+                <textarea
+                    id={props.id}
+                    rows={props.rows || 3}
+                    onChange={changeHandler}
+                    value={inputState.value}
+                />
             );
 
     return (
-        <div className={`form-control`}>
+        <div className={`form-control ${!inputState.isValid &&
+            'form-control--invalid'}`}
+        >
             <label htmlFor={props.id}>{props.label}</label>
             {element}
+            {!inputState.isValid && <p>{props.errorText}</p>}
         </div>
     );
 };
