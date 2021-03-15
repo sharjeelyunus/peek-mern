35f3a0c88850db899f25b5c341af7d0072354cce
sharjeelyunus <sharjeel924@gmail.com>
2021-03-16 01:07:48 +0500

    📦 NEW: Sending a POST Request to the Backend

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -55,9 +55,33 @@ const Auth = () => {
         setIsLoginMode(prevMode => !prevMode);
     };
 
-    const authSubmitHandler = event => {
+    const authSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
+
+        if (isLoginMode) {
+
+        } else {
+            try {
+                const response = await fetch('http://localhost:5000/api/users/signup', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: formState.inputs.name.value,
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    })
+                });
+
+                const responseData = await response.json();
+                console.log(responseData);
+            } catch (err) {
+                console.log(err);
+            }
+
+        }
+
         auth.login();
     };
 
