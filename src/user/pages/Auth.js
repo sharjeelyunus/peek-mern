44d4cb9e29677ba79b32d41433cd8b2dc1d49e15
sharjeelyunus <sharjeel924@gmail.com>
2021-03-19 19:25:20 +0500

    👌 IMPROVE: Proper Error Handling

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -79,6 +79,9 @@ const Auth = () => {
                 });
 
                 const responseData = await response.json();
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
                 console.log(responseData);
                 setIsLoading(false);
                 auth.login();
@@ -89,48 +92,53 @@ const Auth = () => {
             }
         }
     };
-
+    const errorHandler = () => {
+        setError(null);
+    };
     return (
-        <Card className="authentication">
-            {isLoading && <LoadingSpinner asOverlay />}
-            <h2>Login Required</h2>
-            <hr />
-            <form onSubmit={authSubmitHandler}>
-                {!isLoginMode && (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={errorHandler} />
+            <Card className="authentication">
+                {isLoading && <LoadingSpinner asOverlay />}
+                <h2>Login Required</h2>
+                <hr />
+                <form onSubmit={authSubmitHandler}>
+                    {!isLoginMode && (
+                        <Input
+                            element="input"
+                            id="name"
+                            type="text"
+                            label="Your Name"
+                            validators={[VALIDATOR_REQUIRE()]}
+                            errorText="Please enter a name."
+                            onInput={inputHandler}
+                        />
+                    )}
                     <Input
                         element="input"
-                        id="name"
-                        type="text"
-                        label="Your Name"
-                        validators={[VALIDATOR_REQUIRE()]}
-                        errorText="Please enter a name."
+                        id="email"
+                        type="email"
+                        label="E-Mail"
+                        validators={[VALIDATOR_EMAIL()]}
+                        errorText="Please enter a valid email address."
                         onInput={inputHandler}
                     />
-                )}
-                <Input
-                    element="input"
-                    id="email"
-                    type="email"
-                    label="E-Mail"
-                    validators={[VALIDATOR_EMAIL()]}
-                    errorText="Please enter a valid email address."
-                    onInput={inputHandler}
-                />
-                <Input
-                    element="input"
-                    id="password"
-                    type="password"
-                    label="Password"
-                    validators={[VALIDATOR_MINLENGTH(5)]}
-                    errorText="Please enter a valid password, at least 5 characters"
-                    onInput={inputHandler}
-                />
-                <Button type="submit" disabled={!formState.isValid}>
-                    {isLoginMode ? 'LOGIN' : 'SIGNUP'}
-                </Button>
-            </form>
-            <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
-        </Card>
+                    <Input
+                        element="input"
+                        id="password"
+                        type="password"
+                        label="Password"
+                        validators={[VALIDATOR_MINLENGTH(5)]}
+                        errorText="Please enter a valid password, at least 5 characters"
+                        onInput={inputHandler}
+                    />
+                    <Button type="submit" disabled={!formState.isValid}>
+                        {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+                    </Button>
+                </form>
+                <Button inverse onClick={switchModeHandler}>SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}</Button>
+            </Card>
+        </React.Fragment>
     );
 };
 
