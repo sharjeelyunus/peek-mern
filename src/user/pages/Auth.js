30778814f4e1b0ef34a7545e05ade9b50580e01c
sharjeelyunus <sharjeel924@gmail.com>
2021-03-20 19:18:47 +0500

    👌 IMPROVE: Sending a Login Request

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -61,11 +61,34 @@ const Auth = () => {
     const authSubmitHandler = async event => {
         event.preventDefault();
 
-        if (isLoginMode) {
+        setIsLoading(true);
 
+        if (isLoginMode) {
+            try {
+                const response = await fetch('http://localhost:5000/api/users/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    })
+                });
+
+                const responseData = await response.json();
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+                setIsLoading(false);
+                auth.login();
+            } catch (err) {
+                console.log(err);
+                setIsLoading(false);
+                setError(err.message || 'Something went wrong, please try again.');
+            }
         } else {
             try {
-                setIsLoading(true);
                 const response = await fetch('http://localhost:5000/api/users/signup', {
                     method: 'POST',
                     headers: {
@@ -82,7 +105,6 @@ const Auth = () => {
                 if (!response.ok) {
                     throw new Error(responseData.message);
                 }
-                console.log(responseData);
                 setIsLoading(false);
                 auth.login();
             } catch (err) {
