3b7d9264e1cbf42a78b53642c457d6f3eadff3c4
sharjeelyunus <sharjeel924@gmail.com>
2021-03-21 18:01:08 +0500

    👌 IMPROVE: Getting Users with a GET Request

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -22,7 +22,7 @@ const UsersList = props => {
                 id={user.id}
                 image={user.image}
                 name={user.name}
-                placeCount={user.places}
+                placeCount={user.places.length}
             />
         ))}
     </ul>
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,17 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIelements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIelements/LoadingSpinner';
 
 const Users = () => {
-    const USERS = [
-        {
-            id: 'u1',
-            name: 'Sharjeel',
-            image: 'https://www.dictionary.com/e/wp-content/uploads/2010/08/55_Mosque-790x310.jpg',
-            places: 3
-        }];
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [loadedUsers, setLoadedUsers] = useState();
 
-    return <UsersList items={USERS} />;
+
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('http://localhost:5000/api/users');
+
+                const responseData = await response.json();
+
+                if (!response.ok) {
+                    throw new Error(responseData.message);
+                }
+
+                setLoadedUsers(responseData.users);
+            } catch (err) {
+
+                setError(err.message);
+            }
+            setIsLoading(false);
+
+        };
+        sendRequest();
+    }, []);
+
+    const errorHandler = () => {
+        setError(null);
+    };
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onCLear={errorHandler} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedUsers && <UsersList items={loadedUsers} />}
+        </React.Fragment>
+    );
 };
 
 export default Users;
