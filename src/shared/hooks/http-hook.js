5151515a53579c250859bcf18edf6a6c73db829c
sharjeelyunus <sharjeel924@gmail.com>
2021-03-22 00:37:25 +0500

    📦 NEW: Creating a Custom Http Hook

---
+++ src/shared/hooks/http-hook.js
@@ -0,0 +1,44 @@
+import { useState, useCallback, useRef, useEffect } from 'react';
+
+export const useHttpClient = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const activeHttpRequests = useRef([]);
+
+    const sendRequest = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        setIsLoading(true);
+        const httpAbortCtrl = new AbortController();
+        activeHttpRequests.current.push(httpAbortCtrl);
+        try {
+            const response = await fetch(url, {
+                method,
+                body,
+                headers,
+                signal: httpAbortCtrl.signal
+            });
+
+            const responseData = await response.json();
+            if (!response.ok) {
+                throw new Error(responseData.message);
+            }
+
+            return responseData;
+        } catch (err) {
+            setError(err.message);
+        }
+        setIsLoading(false);
+    }, []);
+
+    const clearError = () => {
+        setError(null);
+    };
+
+    useEffect(() => {
+        return () => {
+            activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());
+        };
+    }, []);
+
+    return { isLoading, error, sendRequest, clearError };
+};
