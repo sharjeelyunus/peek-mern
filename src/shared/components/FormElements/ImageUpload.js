65dcac78644ac59abab467d561a0f8deaf76f623
sharjeelyunus <sharjeel924@gmail.com>
2021-03-31 00:31:01 +0500

    📦 NEW: Finalizing Image Upload

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -1,13 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import Button from './Button';
 import './ImageUpload.css';
 
 const ImageUpload = props => {
+    const [file, setFile] = useState();
+    const [previewUrl, setPreviewUrl] = useState();
+    const [isValid, setIsValid] = useState(false);
+
     const filePickerRef = useRef();
 
+    useEffect(() => {
+        if (!file) {
+            return;
+        }
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPreviewUrl(fileReader.result);
+        };
+        fileReader.readAsDataURL(file);
+    }, [file]);
+
     const pickedHandler = event => {
-        console.log(event.target);
+        let pickedFile;
+        let fileIsValid = isValid;
+        if (event.target.files && event.target.files.length === 1) {
+            pickedFile = event.target.files[0];
+            setFile(pickedFile);
+            isValid(true);
+            fileIsValid = true;
+        } else {
+            setIsValid(false);
+            fileIsValid = false;
+        }
+        props.onInput(props.id, pickedFile, fileIsValid);
     };
 
     const pickImageHandler = () => {
@@ -26,10 +52,12 @@ const ImageUpload = props => {
             />
             <div className={`image-upload ${props.center && 'center'}`}>
                 <div className="image-upload__preview">
-                    <img src="" alt="Preview" />
+                    {previewUrl && <img src={previewUrl} alt="Preview" />}
+                    {!previewUrl && <p>Please Pick an image.</p>}
                 </div>
                 <Button type="button" onClick={pickImageHandler}>PICK IMAGE</Button>
             </div>
+            {!isValid && <p>{props.errorText}</p>}
         </div>
     );
 };
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -40,7 +40,8 @@ const Auth = () => {
             setFormData(
                 {
                     ...formState.inputs,
-                    name: undefined
+                    name: undefined,
+                    image: undefined
                 },
                 formState.inputs.email.isValid && formState.inputs.password.isValid
             );
@@ -50,6 +51,10 @@ const Auth = () => {
                     ...formState.inputs,
                     name: {
                         value: '',
+                        isValid: false
+                    },
+                    image: {
+                        value: null,
                         isValid: false
                     }
                 },
@@ -61,6 +66,8 @@ const Auth = () => {
 
     const authSubmitHandler = async event => {
         event.preventDefault();
+
+        console.log(formState.inputs);
 
         if (isLoginMode) {
             try {
@@ -110,7 +117,9 @@ const Auth = () => {
                             onInput={inputHandler}
                         />
                     )}
-                    {!isLoginMode && <ImageUpload center id="image" />}
+                    {!isLoginMode && (
+                        <ImageUpload center id="image" onInput={inputHandler} />
+                    )}
                     <Input
                         element="input"
                         id="email"
