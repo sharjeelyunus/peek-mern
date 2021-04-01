68373edf46d989c3e106788a16e1267cb0198bbc
sharjeelyunus <sharjeel924@gmail.com>
2021-04-02 01:37:35 +0500

    📦 NEW: Wiring Frontend and Backend Up

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -67,14 +67,15 @@ const Auth = () => {
     const authSubmitHandler = async event => {
         event.preventDefault();
 
-        console.log(formState.inputs);
-
         if (isLoginMode) {
             try {
-                const responseData = await sendRequest('http://localhost:5000/api/users/login', 'POST', JSON.stringify({
-                    email: formState.inputs.email.value,
-                    password: formState.inputs.password.value
-                }),
+                const responseData = await sendRequest(
+                    'http://localhost:5000/api/users/login',
+                    'POST',
+                    JSON.stringify({
+                        email: formState.inputs.email.value,
+                        password: formState.inputs.password.value
+                    }),
                     {
                         'Content-Type': 'application/json'
                     }
@@ -83,15 +84,15 @@ const Auth = () => {
             } catch (err) { }
         } else {
             try {
-                const responseData = await sendRequest('http://localhost:5000/api/users/signup', 'POST', JSON.stringify({
-                    name: formState.inputs.name.value,
-                    email: formState.inputs.email.value,
-                    password: formState.inputs.password.value
-                }),
-                    {
-                        'Content-Type': 'application/json'
-                    },
-
+                const formData = new FormData();
+                formData.append('email', formState.inputs.email.value);
+                formData.append('name', formState.inputs.name.value);
+                formData.append('password', formState.inputs.password.value);
+                formData.append('image', formState.inputs.image.value);
+                const responseData = await sendRequest(
+                    'http://localhost:5000/api/users/signup',
+                    'POST',
+                    formData
                 );
 
                 auth.login(responseData.user.id);
