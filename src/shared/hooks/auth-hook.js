061ecda9a92ec764256b284183cb411348c47d32
sharjeelyunus <sharjeel924@gmail.com>
2021-04-19 00:03:30 +0500

    📦 NEW: Creating a Custom Authentication Hook

---
+++ src/shared/hooks/auth-hook.js
@@ -0,0 +1,54 @@
+import { useState, useCallback, useEffect } from 'react';
+
+let logoutTimer;
+
+export const useAuth = () => {
+    const [token, setToken] = useState(false);
+    const [tokenExpirationDate, setTokenExpirationDate] = useState();
+    const [userId, setUserId] = useState(false);
+
+    const login = useCallback((uid, token, expirationDate) => {
+        setToken(token);
+        setUserId(uid);
+        const tokenExpirationDate =
+            expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+        setTokenExpirationDate(tokenExpirationDate);
+        localStorage.setItem(
+            'userData',
+            JSON.stringify({
+                userId: uid,
+                token: token,
+                expiration: tokenExpirationDate.toISOString()
+            })
+        );
+    }, []);
+
+    const logout = useCallback(() => {
+        setToken(null);
+        setTokenExpirationDate(null);
+        setUserId(null);
+        localStorage.removeItem('userData');
+    }, []);
+
+    useEffect(() => {
+        if (token && tokenExpirationDate) {
+            const remainingTime = tokenExpirationDate.getTime() - new Date().getTime();
+            logoutTimer = setTimeout(logout, remainingTime);
+        } else {
+            clearTimeout(logoutTimer);
+        }
+    }, [token, logout, tokenExpirationDate]);
+
+    useEffect(() => {
+        const storedData = JSON.parse(localStorage.getItem('userData'));
+        if (
+            storedData &&
+            storedData.token &&
+            new Date(storedData.expiration) > new Date()
+        ) {
+            login(storedData.userId, storedData.token, new Date(storedData.expiration));
+        }
+    }, [login]);
+
+    return { token, login, logout, userId };
+};
