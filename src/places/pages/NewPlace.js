e71da908225f06cc8854ecc61246eed3ed452201
sharjeelyunus <sharjeel924@gmail.com>
2021-04-20 00:28:49 +0500

    👌 IMPROVE:  General App Improvements

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -49,7 +49,6 @@ const NewPlace = () => {
       formData.append('title', formState.inputs.title.value);
       formData.append('description', formState.inputs.description.value);
       formData.append('address', formState.inputs.address.value);
-      formData.append('creator', auth.userId);
       formData.append('image', formState.inputs.image.value);
       await sendRequest('http://localhost:5000/api/places', 'POST', formData, {
         Authorization: 'Bearer ' + auth.token
